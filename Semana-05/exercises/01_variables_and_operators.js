45daa7089b7a46783a59771a1241f9083004c91d
Gonzalo Asencio <gestorgonzalo@gmail.com>
2022-09-18 00:33:29 -0300

    BaSp-W05-2022: ex01-variables and operators

---
+++ Semana-05/exercises/01_variables_and_operators.js
@@ -0,0 +1,27 @@
+console.log('-- Exercise 1: Variables and Operators');
+
+//1.a) Crear dos variables numéricas y utilizar el operador suma para guardar el valor de la suma de ambos números en una 3er variable.
+
+console.log('-Exercise 1.a:');
+var firstNumber = 2;
+var secondNumber = 2;
+var sum = firstNumber + secondNumber;
+console.log(sum);
+
+//1.b)Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er variable.
+
+console.log('-Exercise 1.b:');
+
+var firstName = 'Gonzalo ';
+var lastName = 'Asencio';
+var concatenation = firstName + lastName;
+console.log(concatenation);
+
+//1.c)Crear dos variables de tipo String y sumar el largo de cada variable (cantidad de letras del string) guardando el resultado de la suma en una 3er variable (utilizar length).
+
+console.log('-Exercise 1.c:');
+
+var firstWord = 'Radium';
+var secondWord = 'Rocket';
+var sum = firstWord.length + secondWord.length;
+console.log(sum);
