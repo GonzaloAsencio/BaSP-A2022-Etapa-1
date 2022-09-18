cc36d5fd839116c2947fde8f917a7f127c2d02a4
Gonzalo Asencio <gestorgonzalo@gmail.com>
2022-09-18 01:40:27 -0300

    BaSp-W05-2022: ex04-If Else.

--- Semana-05/exercises/03_arrays.js
+++ Semana-05/exercises/03_arrays.js
@@ -7,32 +7,38 @@ var months =  ["January", "February ", "March", "April", "May", "June", "July", 
 console.log("Month 5 and 11:" , months[4], months[10]);
 
 //3.b) Ordenar el array de meses alfabéticamente y mostrarlo por consola (utilizar sort).*/
+
 console.log('-Exercise 3.b:');
 months.sort();
 console.log("Order months:",months);
 
 //3.c)Agregar un elemento al principio y al final del array (utilizar unshift y push).
+
 console.log('-Exercise 3.c:');
 months.unshift("First");
 months.push("Last");
 console.log("Added months:", months);
 
 //3.d)Quitar un elemento del principio y del final del array (utilizar shift y pop).
+
 console.log('-Exercise 3.d:');
 months.shift("First");
 months.pop("Last");
 console.log("Removed months:", months);
 
 //3.e)Invertir el orden del array (utilizar reverse).
+
 console.log('-Exercise 3.e:');
 months.reverse();
 console.log("Reverse months", months);
 
 //3.f)Unir todos los elementos del array en un único string donde cada mes este separado por un guión - (utilizar join).
+
 console.log('-Exercise 3.f:');
 console.log("Join months:" , months.join('-'));
 
 //3.g)Crear una copia del array de meses que contenga desde Mayo hasta Noviembre (utilizar slice).
+
 console.log('-Exercise 3.g:');
 var slicedMonths = months.slice(4,11);
 console.log("Sliced Months", slicedMonths);
---
+++ Semana-05/exercises/04_if_else.js
@@ -0,0 +1,44 @@
+console.log('-- Exercise 4: If Else');
+
+//4.a)Crear un número aleatorio entre 0 y 1 utilizando la función Math.random(), si el valor es mayor o igual que 0,5 mostrar una alerta con el mensaje “Greater than 0,5” y sino un alerta con el mensaje “Lower than 0,5”.
+
+console.log('-Exercise 4.a:');
+var randomNumber = Math.random(0,1);
+if (randomNumber >= 0.5) {
+    console.log("Greater than 0,5"); 
+}else{
+    console.log("Lower than 0,5"); 
+}
+
+//4.b)Crear una variable “Age” que contenga un número entero entre 0 y 100 y muestre los siguientes mensajes de alerta:
+//i.“Bebe” si la edad es menor a 2 años;
+//ii.“Niño” si la edad es entre 2 y 12 años;
+//iii.“Adolescente” entre 13 y 19 años;
+//iv.“Joven” entre 20 y 30 años;
+//v.“Adulto” entre 31 y 60 años;
+//vi.“Adulto mayor” entre 61 y 75 años;
+//vii.“Anciano” si es mayor a 75 años.*/
+
+console.log('-Exercise 4.b:');
+var Age = Math.floor(Math.random() * 100);
+console.log(Age);
+if(Age < 2){
+    console.log("Baby");
+}else if (Age >=2 && Age <=12){
+    console.log("Children");
+}
+else if (Age >=13 && Age<=19){
+    console.log("Teen");
+}
+else if (Age >=20 && Age<=30){
+    console.log("Young man");
+}
+else if (Age >=31 && Age<=60){
+    console.log("Adult");
+}
+else if (Age >=61 && Age<=75){
+    console.log("Senior adult");
+}
+else if (Age > 75){
+    console.log("Old man");
+}
